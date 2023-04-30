73e605eaa6c6627294d3532d7693db7cc9e3f23e
GdonatasG <zitkusd@gmail.com>
2023-04-30 18:16:06 +0300

    Create ProjectsScreen with TabRow & Create ListTile component

---
+++ android/components/src/main/java/com/project/space/components/ListTile.kt
@@ -0,0 +1,60 @@
+package com.project.space.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ListTile(
+    title: String,
+    description: String? = null,
+    divided: Boolean = true,
+    onClick: (() -> Unit)? = null,
+    trailing: @Composable () -> Unit = {}
+) {
+    var modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .defaultMinSize(minHeight = 60.dp)
+
+    onClick?.let {
+        modifier = modifier.clickable { it() }
+    }
+
+    Column {
+        Row(
+            modifier = modifier.padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                description?.let {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.bodySmall,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+
+            }
+            trailing()
+        }
+        if (divided) {
+            Divider()
+        }
+    }
+}
--- feature/projects/android-ui/src/main/java/com/project/space/feature/projects/android_ui/ProjectsScreen.kt
+++ feature/projects/android-ui/src/main/java/com/project/space/feature/projects/android_ui/ProjectsScreen.kt
@@ -1,23 +1,76 @@
 package com.project.space.feature.projects.android_ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowRight
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import com.project.space.components.ListTile
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProjectsScreen(viewModel: ProjectsViewModel) {
-    Scaffold(
-        modifier = Modifier
-            .statusBarsPadding()
-            .navigationBarsPadding()
-    ) {
-        Text(text = "ProjectsScreen")
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    val tabs: List<String> = listOf("My projects", "Others")
+
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .navigationBarsPadding()
+                .imePadding()
+                .captionBarPadding()
+        ) {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Projects") },
+                actions = {
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Image(imageVector = Icons.Default.Add, contentDescription = "Create Project")
+                    }
+                }
+            )
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(title) }, selected = index == selectedTabIndex,
+                        onClick = {
+                            selectedTabIndex = index
+                        }
+                    )
+                }
+            }
+/*            ListTile(
+                title = "TestTitle",
+                description = "Some dummy description that is very very long and doesn't fit in one line and overflows, " +
+                        "Some dummy description that is very very long and doesn't fit in one line and overflows",
+                onClick = {
+
+                },
+                trailing = { Image(imageVector = Icons.Default.ArrowRight, contentDescription = "Change project") }
+            )
+            ListTile(
+                title = "TestTitle",
+                description = "Some dummy description that is very very long and doesn't fit in one line and overflows, " +
+                        "Some dummy description that is very very long and doesn't fit in one line and overflows",
+                onClick = {
+
+                },
+                trailing = { Image(imageVector = Icons.Default.ArrowRight, contentDescription = "Change project") },
+                divided = false
+            )*/
+        }
     }
 }
